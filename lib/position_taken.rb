386d4c2957c35b067eb91c48a15512e2bd33d94f
valeria campilongo <cvaleriac@hotmail.it>
2019-12-09 18:39:08 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-<<<<<<< HEAD
+
 def input_to_index (index)
  index = index.to_i
  index = index - 1
@@ -16,22 +16,22 @@ end
 #  index = index.to_i
 #  index = index - 1
 #end
->>>>>>> d2692c471d9530448fed4ca9723d4ffde6056f48
+
 
 def move (board, index, value = "X")
   board[index.to_i] = value
 end
 
 def position_taken?(board, index)
-<<<<<<< HEAD
+
   if (board[index] == " " || board[index] == "" || board[index] == nil)
     return false
   else #(board[index] == "X" || board[index]== "O")
-=======
+
   if board[index] == " " || "" || nil
     return false
   elsif board[index] == "X" || "O"
->>>>>>> d2692c471d9530448fed4ca9723d4ffde6056f48
+
     return true
   end
 end
